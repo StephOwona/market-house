8f47392c117199d8648662eaff756f5c1ef91f5c
Stephane <stephowona05@gmail.com>
2026-03-10 20:43:41 +0100

    fix: Utilisation de /tmp pour la DBsur vercel

--- database/db.py
+++ database/db.py
@@ -3,7 +3,10 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
-DB_PATH = os.path.join(BASE_DIR, "market_house.db")
+if os.environ.get("VERCEL"):
+    DB_PATH = "/tmp/market_house.db"
+else:
+    DB_PATH = os.path.join(BASE_DIR, "market_house.db")
 DATABASE_URL = f"sqlite:///{DB_PATH}"
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
--- wsgi.py
+++ wsgi.py
@@ -1,9 +1,10 @@
 from backend.app import create_app
 from database.seed import seed
+from database.db import DB_PATH
 import os
 
 # Initialiser la DB si elle n'existe pas
-if not os.path.exists("market_house.db"):
+if not os.path.exists(DB_PATH):
     seed()
 
 app = create_app()
